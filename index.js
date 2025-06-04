206a6cd8fdf4e561c474e3ce73f65449dc029c7c
ZahraRezai72500 <rezayizahra1560@gmail.com>
2025-06-04 06:51:20 -0700

    fix it

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ import cors from 'cors';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const allowedOrigings =['http://localhost:4200']
+const allowedOrigings = ['http://localhost:4200','https://afghan-proverbs-front-hspd.onrender.com']
 app.use(cors({
     origin: allowedOrigings
 }))
-app.use ('/api/proverbs',proverbsRoutes);
+app.use ('/api/proverbs', proverbsRoutes);
 
 app.listen(PORT,()=>{
     console.log(`Afghan Proverbs API running on PORT ${PORT}`);
